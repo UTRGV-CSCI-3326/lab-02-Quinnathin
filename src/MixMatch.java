310da541b31e39345c44ac14fa6472a20eccdb6d
Quinnathin <quinnboyer475@gmail.com>
2025-02-05 02:01:12 +0000

    MixMatchDone

---
+++ src/MixMatch.java
@@ -0,0 +1,30 @@
+public class MixMatch{
+    public static void main(String args[]){
+        String userName = "Quinn";
+        System.out.println("userName: " + userName);
+
+        char whatMarksTheSpot = 'x';
+        System.out.println("whatMarksTheSpot: " + whatMarksTheSpot);
+
+        short numberOfCats = 2;
+        System.out.println("numberOfCats: " + numberOfCats);
+
+        int totalMilage = 100012;
+        System.out.println("totalMilage: " + totalMilage);
+
+        long squareFeet = 2048000005;
+        System.out.println("squareFeet: " + squareFeet);
+
+        double hoursSlept = 4.5;
+        System.out.println("hoursSlept: " + hoursSlept);
+
+        float pi = 3.1415926f;
+        System.out.println("pi: " + pi);
+
+        boolean indoors = true;
+        System.out.println("indoors? " + indoors); 
+
+        System.out.println("My name's " + userName + " and I have " + numberOfCats + " cats.");
+        System.out.println(hoursSlept + " hours slept? " + indoors);
+    }
+}
